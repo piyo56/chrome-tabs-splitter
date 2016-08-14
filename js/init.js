a9bba90460572a842e6f33b4eba1cfcba4fa3c80
piyo56 <piyo56.net@gmail.com>
2016-08-14 23:09:08 +0900

    [Update] ボタンに押し込むデザインを追加

--- js/init.js
+++ js/init.js
@@ -5,6 +5,7 @@ window.onload = function() {
     if (!localStorage) {
         store_button.style.display   = "none";
         retrieve_button.style.display = "none";
+        document.getElementById("func_buttons").style.width = "180px";
         return;
     } 
     
@@ -12,7 +13,23 @@ window.onload = function() {
     var item = localStorage.getItem("temporarily_stored_tabs");
     if(item === null) {
         retrieve_button.style.display = "none";
+        document.getElementById("func_buttons").style.width = "275px";
     } else {
         store_button.style.display    = "none";
+        document.getElementById("func_buttons").style.width = "315px";
+    }
+    
+    //ボタンにおしこむようなデザインを追加
+    var buttons = ["split", "store", "bookmark", "save"];
+    for(var i=0; i<buttons.length; i++) {
+        document.getElementById(buttons[i]).addEventListener("mousedown", function(e){
+            //class追加
+            this.classList.add("pushed");
+        });
+        document.getElementById(buttons[i]).addEventListener("mouseup", function(e){
+            //class削除
+            this.classList.remove("pushed");
+        });
     }
 };
+
